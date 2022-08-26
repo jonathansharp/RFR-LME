97dcc6f6837850edf0be37fff366aa12e5a043a3
Jon Sharp <jonathan.sharp@noaa.gov>
2022-08-26 10:23:56 -0700

    Add plotting function

---
+++ plot_temporal_mean.m
@@ -0,0 +1,17 @@
+% Plot temporal mean of spatial surface variable
+function plot_temporal_mean(lim,dim,lat,lon,z,zmin,zmax,lev,varname,lab,reg)
+
+figure; worldmap([lim.latmin lim.latmax],[lim.lonmin lim.lonmax]);
+pcolorm(repmat(lat',dim.x,1),repmat(lon,1,dim.y),mean(z,3,'omitnan'));
+land = shaperead('landareas', 'UseGeoCoords',true);
+geoshow(land,'FaceColor',rgb('grey'));
+c=colorbar;
+colormap(parula(lev));
+caxis([zmin,zmax]);
+c.TickLength = 0;
+c.Label.String = lab;
+cbarrow;
+
+% save figure
+exportgraphics(gcf,['Figures/' reg '_' varname '.png']);
+close
