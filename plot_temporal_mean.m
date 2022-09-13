5c00df628dc26a6aa97e73f97a3067fca44d7a49
Jon Sharp <jonathan.sharp@noaa.gov>
2022-09-13 16:48:25 -0700

    Update plot_temporal_mean.m

--- plot_temporal_mean.m
+++ plot_temporal_mean.m
@@ -1,10 +1,10 @@
 % Plot temporal mean of spatial surface variable
 function plot_temporal_mean(lim,dim,lat,lon,z,zmin,zmax,lev,varname,lab,reg)
 
-figure; worldmap([lim.latmin lim.latmax],[lim.lonmin lim.lonmax]);
+figure('visible','off'); worldmap([lim.latmin lim.latmax],[lim.lonmin lim.lonmax]);
 pcolorm(repmat(lat',dim.x,1),repmat(lon,1,dim.y),mean(z,3,'omitnan'));
 land = shaperead('landareas', 'UseGeoCoords',true);
-geoshow(land,'FaceColor',rgb('grey'));
+% geoshow(land,'FaceColor',rgb('grey'));
 c=colorbar;
 colormap(parula(lev));
 caxis([zmin,zmax]);
