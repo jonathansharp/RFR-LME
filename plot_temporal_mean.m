d2b86ee2b6c61daadd1d00b1336dd96a104f3e64
Jon Sharp <jonathan.sharp@noaa.gov>
2022-10-06 11:22:53 -0700

    Update plot_temporal_mean.m

--- plot_temporal_mean.m
+++ plot_temporal_mean.m
@@ -9,7 +9,7 @@ function plot_temporal_mean(lim,dim,lat,lon,z,zmin,zmax,lev,varname,lab,reg)
 figure('visible','off'); worldmap([lim.latmin lim.latmax],[lim.lonmin lim.lonmax]);
 pcolorm(repmat(lat',dim.x,1),repmat(lon,1,dim.y),mean(z,3,'omitnan'));
 land = shaperead('landareas', 'UseGeoCoords',true);
-% geoshow(land,'FaceColor',rgb('grey'));
+geoshow(land,'FaceColor',rgb('grey'));
 c=colorbar;
 colormap(parula(lev));
 caxis([zmin,zmax]);
