0d6cc09bd5481d552ff7d5988079ca794a2defeb
Jonathan Sharp <jonathan.sharp@noaa.gov>
2023-02-22 08:48:40 -0800

    Feb. 22 update (2)

--- optimization/run_optimization.m
+++ optimization/run_optimization.m
@@ -1,2 +1,7 @@
-% Run scripts to optimize parameters for GMM clustering and RFR model fits
+% Run scripts to optimize parameters for GMM clustering and RFR algorithm fits
 
+% Procedure to test and optimize GMM cluster parameters
+GMM_test
+
+% Procedure to test and optimize RFR cluster parameters
+GMM_test
--- run_workflow.m
+++ run_workflow.m
@@ -1,42 +1,42 @@
-% Create surface pCO2 maps in US LMEs using machine learning algorithms
-% trained with SOCAT data
+% Run scripts to create surface fCO2 maps in US LMEs using machine learning
+% algorithms trained with SOCAT data, create surface TA maps in the same
+% LMEs using ESPER algorithms, then calculate ocean acidification
+% indicators for each LME.
 
-% navigate to correct directory
+% navigate to correct directory (adjust for your system)
 cd /raid/sharp/matlab/US-LMES
 
 % this script defines the bounds of the eighteen LMEs
 define_regions
 
 % this script loads SOCAT fCO2 and ancillary data surrounding North
-% American extracted from the SOCATv2022 database
-% load_socat
+% America extracted from the SOCATv2022 database
+load_socat
 
 % this script grids fCO2 observations from the SOCAT database into grid
 % cells of resolution: 0.25 deg lat x 0.25 deg lon x 1 month
-% grid_socat
+grid_socat
 
-% this script extracts each of eleven LMEs from the gridded data
+% this script extracts each of eighteen LMEs from the gridded data
 % surrounding North America
-% extract_lme
+extract_lme
 
 % this script loads gridded satellite, model, and reanalysis variables and
 % re-grids them to match the size of the fCO2 grids
-% load_vars
+load_vars
 
 % this script defines predictors variables for algorithm training as X and
 % the target variable for algorithm training (i.e. fCO2) as Y
-% define_x_y
+define_x_y
 
-% Procedure to test and optimize GMM cluster parameters
-% GMM_test
-% Set options (determined via GMM_test)
+% Set options (determined via 'GMM_test' in 'run_optimization.m')
 set_gmm_options
 
 % this script trains self-organizing-maps using the defined predictors to
 % cluster data spatiotemporally for algorithm training
 cluster_on_grid
 
-% Set options (determined via GMM_test)
+% Set options ((determined via 'RFR_test' in 'run_optimization.m'))
 set_rfr_options
 
 % this script trains machine learning algorithms for fCO2 prediction in
